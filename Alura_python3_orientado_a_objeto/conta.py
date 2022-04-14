377c9d0bb433b7b582daab83c83f50f483881c8e
Guilherme de Freitas Anacleto <48931728+Paquista0@users.noreply.github.com>
2022-04-14 12:01:39 -0300

    Add files via upload

--- Alura_python3_orientado_a_objeto/conta.py
+++ Alura_python3_orientado_a_objeto/conta.py
@@ -1,12 +1,32 @@
-#criar objeto
+""" 
+from conta import Conta
+conta = Conta(1, "Gui", 30.0, 1000.0)
+""" 
+
+#criar OBJETO
 class Conta:
 
     #função construtora.
     def __init__(self, numero, titular, saldo, limite): 
-        #self é a referencia que sabe o endereço do objeto na memória.
+        #self é a referencia, ele sabe o endereço do objeto na memória.
         print("Construindo objeto...{}".format(self))
         #self sabe o endereço, então "." = vamos para lá.
-        self.numero = numero
+
         self.titular = titular
         self.saldo = saldo
+        self.numero = numero
         self.limite = limite
+    
+    #funções usadas m teste_procedure.py irão virar METODOS
+    def extrato(self):
+        print("Titular: {} \nSaldo: {} \nNumero: {} \nLimite: {}".format(self.titular, self.saldo, self.numero, self.limite))
+        # conta.extrato()
+        # conta2.extrato()
+
+    def deposita(self, valor):
+        self.saldo += valor
+
+    def saca(self, valor):
+        self.saldo -= valor
+
+
--- Alura_python3_orientado_a_objeto/teste_procedure.py
+++ Alura_python3_orientado_a_objeto/teste_procedure.py
@@ -1,7 +1,9 @@
 def cria_conta(numero,titular,saldo, limite):
     conta = { "numero":numero,"titular":titular, "saldo":saldo, "limite":limite}
     return conta
-
+    #criar conta:
+    #conta = Conta(1, "Gui", 30.0, 1000.0)
+    
 def deposita(conta, valor):
     conta["saldo"] += valor
 
